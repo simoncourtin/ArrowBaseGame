d3ab1be0b1edf120587e6b517ed919ce5141280c
simoncourtin <courtin.simon@gmail.com>
2015-03-11 10:52:57 +0100

    rotation des fleches

--- src/Tir.py
+++ src/Tir.py
@@ -1,7 +1,7 @@
 import os
 
 import pygame
-
+import  math
 import load_png
 import Animable
 
@@ -26,6 +26,7 @@ class Tir(Animable.Animable):
         self.isActive = True
         self.orientation = "droite"
         self.image, self.rect = load_png.load_png(os.path.dirname(__file__) + "/../data/sprite/arrow.png")
+        self.image_base=self.image
         self.origine = origine
         self.rect.center = origine
         self.puissance = puissance / 10
@@ -35,7 +36,7 @@ class Tir(Animable.Animable):
         elif self.puissance < PUISSANCE_MIN:
             self.puissance = PUISSANCE_MIN
 
-        # self.speed = [self.puissance, 0]
+        #self.speed = [self.puissance, 0]
         self.acceleration = [0, 0]
 
         self.speed = [FACTEUR_PUISSANCE * self.puissance * vitesse[0], FACTEUR_PUISSANCE * self.puissance * vitesse[1]]
@@ -46,6 +47,8 @@ class Tir(Animable.Animable):
 
     def update(self):
         self.rect = self.rect.move(self.speed)
+        if self.speed[0]!= 0 or self.speed[1]!= 0:
+            self.image = pygame.transform.rotate(self.image_base,180*math.atan2(-self.speed[1],self.speed[0])/math.pi)
         Animable.Animable.update(self)
 
         # self.acceleration[1] += -VITESSE_DEBUT_LANCER
